41ba10a5e86def667693c0005d1fabd70d00dcbb
apriliaaachi <achiaprilia.aa@gmail.com>
2021-07-28 15:02:51 +0700

    fix gate, app, dan achi

--- gate-ojt.js
+++ gate-ojt.js
@@ -4,14 +4,14 @@ var bodyParser = require('body-parser')
 var port = require('./config/config-port.json')
 
 /* router */
-
+var ojt = require('./gateway/ojt/app')
 
 /* Body Parser */
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
 /* use Function */
-
+router.use('/ojt', ojt)
 
 console.log('The Gateway on port:' + port.gateway)
 router.listen(port.gateway, '0.0.0.0')
