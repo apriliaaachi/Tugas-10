3f937a8d415742d6001758cbf9d96a3e3d38a1ff
apriliaaachi <achiaprilia.aa@gmail.com>
2021-07-30 10:50:35 +0700

    configuration

--- gate-ojt.js
+++ gate-ojt.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var router = express()
 var bodyParser = require('body-parser')
-var port = require('./config/config-port.json')
+//var port = require('./config/config-port.json')
 
 /* router */
-var ojt = require('./gateway/ojt/app')
+var ojt = require('./ojt/app')
 
 /* Body Parser */
 router.use(bodyParser.json())
@@ -13,5 +13,5 @@ router.use(bodyParser.urlencoded({ extended: true }))
 /* use Function */
 router.use('/ojt', ojt)
 
-console.log('The Gateway on port:' + port.gateway)
-router.listen(port.gateway, '0.0.0.0')
+console.log('The Gateway on port:' + 8080)
+router.listen(8080, '0.0.0.0')
