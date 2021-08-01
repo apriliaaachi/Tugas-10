2574ab130d6b2d446535bba7205cf0c8e0608900
apriliaaachi <achiaprilia.aa@gmail.com>
2021-08-01 19:48:42 +0700

    app updated

--- ojt/app.js
+++ ojt/app.js
@@ -4,11 +4,18 @@ var bodyParser = require('body-parser')
 
 /* router */
 var credential = require('./credential')
+/* tugas 1 */
 var priscillia = require('./priscillia')
 var aryo = require('./aryo')
 var hana = require('./hana')
 var bayu = require('./bayu')
 var achi = require('./achi')
+/* tugas 2 */
+var priscillia2 = require('./priscillia2')
+var aryo2 = require('./aryo2')
+var hana2 = require('./hana2')
+var bayu2 = require('./bayu2')
+var achi2 = require('./achi2')
 
 /* Body Parser */
 router.use(bodyParser.json())
@@ -16,10 +23,17 @@ router.use(bodyParser.urlencoded({ extended: true }))
 
 /* use Function */
 router.use('/', credential)
+/* tugas 1 */
 router.use('/priscillia', priscillia)
 router.use('/aryo', aryo)
 router.use('/hana', hana)
 router.use('/bayu', bayu)
 router.use('/achi', achi)
+/* tugas 2 */
+router.use('/priscillia2', priscillia2)
+router.use('/aryo2', aryo2)
+router.use('/hana2', hana2)
+router.use('/bayu2', bayu2)
+router.use('/achi2', achi2)
 
 module.exports = router
