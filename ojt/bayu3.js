98e8513eb01ce58fc7768ba128e5e01ef448004e
Bayu Kharisma <bayukharisma@Bayus-MacBook-Pro.local>
2021-08-03 12:26:06 +0700

    change post to get

--- ojt/bayu3.js
+++ ojt/bayu3.js
@@ -8,43 +8,43 @@ const router = express();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post("/", function (req, res, next) {
+router.get("/", function (req, res, next) {
   const ipInfo = getIP(req);
   const clientip = ipInfo.clientIp;
   const dataReq = {
-    iv: req.body.iv,
-    secret: req.body.secret,
-    ciphertext: req.body.ciphertext,
+    iv: req.params.iv,
+    secret: req.params.secret,
+    ciphertext: req.params.ciphertext,
   };
   console.log(
     "Request to API: " + conf.url.inqA + " " + JSON.stringify(dataReq)
   );
   let dataRes = {};
   if (
-    typeof req.body.iv === "string" &&
-    typeof req.body.secret === "string" &&
-    typeof req.body.ciphertext === "string"
+    typeof req.params.iv === "string" &&
+    typeof req.params.secret === "string" &&
+    typeof req.params.ciphertext === "string"
   ) {
-    if (req.body.iv.length !== 16) {
+    if (req.params.iv.length !== 16) {
       dataRes.rsp = "997";
       dataRes.rspdesc = "Invalid data format, iv length must be 16 character.";
-    } else if (req.body.secret.length !== 32) {
+    } else if (req.params.secret.length !== 32) {
       dataRes.rsp = "997";
       dataRes.rspdesc = "Invalid data format, secret length must be 32 character.";
     } else {
       const AES256CBC = CryptoJS.AES.decrypt(
         CryptoJS.lib.CipherParams.create({
-          ciphertext: CryptoJS.enc.Base64.parse(req.body.ciphertext),
+          ciphertext: CryptoJS.enc.Base64.parse(req.params.ciphertext),
           formatter: CryptoJS.format.OpenSSL,
         }),
-        CryptoJS.enc.Utf8.parse(req.body.secret),
-        { iv: CryptoJS.enc.Utf8.parse(req.body.iv) }
+        CryptoJS.enc.Utf8.parse(req.params.secret),
+        { iv: CryptoJS.enc.Utf8.parse(req.params.iv) }
       );
       dataRes.rsp = "000";
       dataRes.rspdesc = "Success";
-      dataRes.iv = req.body.iv;
-      dataRes.secret = req.body.secret;
-      dataRes.ciphertext = req.body.ciphertext;
+      dataRes.iv = req.params.iv;
+      dataRes.secret = req.params.secret;
+      dataRes.ciphertext = req.params.ciphertext;
       dataRes.plaintext = AES256CBC.toString(CryptoJS.enc.Utf8);
     }
   } else {
